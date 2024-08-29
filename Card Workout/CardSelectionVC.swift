8a75e439ba9e386429d1ed179af710f4a7fabbbb
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-29 16:53:20 +0300

    Created the UI for the main storyboard

--- Card Workout/CardSelectionVC.swift
+++ Card Workout/CardSelectionVC.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardSelectionVC: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    
+    
+    // MARK: - Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
