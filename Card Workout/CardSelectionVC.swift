4e46de106bc06b04dbcf2aa739052c573c67f017
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-29 17:23:23 +0300

    Created Rules View controller

--- Card Workout/CardSelectionVC.swift
+++ Card Workout/CardSelectionVC.swift
@@ -11,16 +11,38 @@ class CardSelectionVC: UIViewController {
     
     // MARK: - IBOutlets
     
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
     
-    
-    // MARK: - Functions
+    // MARK: - VC Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
         
     }
 
-
+    // MARK: - Functions
+    
+    
+    
+    // MARK: - IBActions
+    
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
