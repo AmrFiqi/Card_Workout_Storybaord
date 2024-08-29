8cadc743accb555fdd23d939c10e5c1bb5e0282d
AmrFiqi <amr.h.elfiqi@gmail.com>
2024-08-29 18:10:59 +0300

    Added Card struct which contains all cards in assets and created the logic of the game

--- Card Workout/CardSelectionVC.swift
+++ Card Workout/CardSelectionVC.swift
@@ -14,35 +14,51 @@ class CardSelectionVC: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
     
+    
+    // MARK: - Variables
+    
+    var timer: Timer!
+    var cards: [UIImage] = Card.allValues
+    
     // MARK: - VC Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startTimer()
         for button in buttons {
             button.layer.cornerRadius = 8
         }
-        
     }
+        
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            timer.invalidate()
+    }
+        
 
     // MARK: - Functions
     
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
     
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
     
     // MARK: - IBActions
     
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        
+        timer.invalidate()
     }
     
     
     @IBAction func refreshButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
-    
-    
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
-    }
-    
+
 }
 
